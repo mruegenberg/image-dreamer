855ec80111d18692a787dc7e633bb0e0b2cd4877
Marcel Ruegenberg <ruegenbe@in.tum.de>
2015-11-03 10:35:33 +0100

    Tuned

--- dreamify.py
+++ dreamify.py
@@ -7,8 +7,12 @@ from IPython.display import clear_output, Image, display
 from google.protobuf import text_format
 
 import caffe
-
+import math
 import sys
+
+# uncomment for GPU.
+# caffe.set_mode_gpu()
+# caffe.set_device(0) # select GPU 
 
 def savearray(a, filename, fmt='png'):
     a = np.uint8(np.clip(a, 0, 255))
@@ -27,79 +31,94 @@ text_format.Merge(open(net_fn).read(), model)
 model.force_backward = True
 open('tmp.prototxt', 'w').write(str(model))
 
-net = caffe.Classifier('tmp.prototxt', param_fn,
-                       mean = np.float32([104.0, 116.0, 122.0]), # ImageNet mean, training set dependent
-                       channel_swap = (2,1,0)) # the reference model has channels in BGR order instead of RGB
+rand = 125
+np.random.seed(rand)
 
-# a couple of utility functions for converting to and from Caffe's input image layout
-def preprocess(net, img):
-    return np.float32(np.rollaxis(img, 2)[::-1]) - net.transformer.mean['data']
-def deprocess(net, img):
-    return np.dstack((img + net.transformer.mean['data'])[::-1])
+def triangleWave(t, period=100):
+    tDivPeriod = t / float(period)
+    return abs(2 * (tDivPeriod - math.floor(tDivPeriod + 0.5)))
 
-def make_step(net, step_size=1.5, end='inception_4c/output', jitter=32, clip=True):
-    '''Basic gradient ascent step.'''
+offset1 = 0
+offset2 = 0
+for foo in range(0,300):
+    addV = foo % 2
+    addV2 = math.floor(foo / 64)
+    offset1 = int(offset1 + addV + addV2)
+    offset2 = int(offset2 + (1 - addV) + addV2)
 
-    src = net.blobs['data'] # input image is stored in Net's 'data' blob
-    dst = net.blobs[end]
+    net = caffe.Classifier('tmp.prototxt', param_fn,
+                        mean = np.float32([104.0, 116.0, 122.0]), # ImageNet mean, training set dependent
+                        channel_swap = (2,1,0)) # the reference model has channels in BGR order instead of RGB
 
-    ox, oy = np.random.randint(-jitter, jitter+1, 2)
-    src.data[0] = np.roll(np.roll(src.data[0], ox, -1), oy, -2) # apply jitter shift
+    # a couple of utility functions for converting to and from Caffe's input image layout
+    def preprocess(net, img):
+        return np.float32(np.rollaxis(img, 2)[::-1]) - net.transformer.mean['data']
+    def deprocess(net, img):
+        return np.dstack((img + net.transformer.mean['data'])[::-1])
 
-    net.forward(end=end)
-    dst.diff[:] = dst.data  # specify the optimization objective
-    net.backward(start=end)
-    g = src.diff[0]
-    # apply normalized ascent step to the input image
-    src.data[:] += step_size/np.abs(g).mean() * g
+    def make_step(net, step_size=1.5, end='inception_4c/output', jitter=32, clip=True):
+        '''Basic gradient ascent step.'''
 
-    src.data[0] = np.roll(np.roll(src.data[0], -ox, -1), -oy, -2) # unshift image
+        src = net.blobs['data'] # input image is stored in Net's 'data' blob
+        dst = net.blobs[end]
 
-    if clip:
-        bias = net.transformer.mean['data']
-        src.data[:] = np.clip(src.data, -bias, 255-bias)
+        ox, oy = (-32 + offset1, -32 + offset2) # np.random.randint(-jitter, jitter+1, 2)
+        src.data[0] = np.roll(np.roll(src.data[0], ox, -1), oy, -2) # apply jitter shift
 
-def deepdream(net, base_img, iter_n=10, octave_n=4, octave_scale=1.4, end='inception_4c/output', clip=True, **step_params):
-    # prepare base images for all octaves
-    octaves = [preprocess(net, base_img)]
-    for i in xrange(octave_n-1):
-        octaves.append(nd.zoom(octaves[-1], (1, 1.0/octave_scale,1.0/octave_scale), order=1))
+        net.forward(end=end)
+        dst.diff[:] = dst.data  # specify the optimization objective
+        net.backward(start=end)
+        g = src.diff[0]
+        # apply normalized ascent step to the input image
+        src.data[:] += step_size/np.abs(g).mean() * g
 
-    src = net.blobs['data']
-    detail = np.zeros_like(octaves[-1]) # allocate image for network-produced details
-    for octave, octave_base in enumerate(octaves[::-1]):
-        h, w = octave_base.shape[-2:]
-        if octave > 0:
-            # upscale details from the previous octave
-            h1, w1 = detail.shape[-2:]
-            detail = nd.zoom(detail, (1, 1.0*h/h1,1.0*w/w1), order=1)
+        src.data[0] = np.roll(np.roll(src.data[0], -ox, -1), -oy, -2) # unshift image
 
-        src.reshape(1,3,h,w) # resize the network's input image size
-        src.data[0] = octave_base+detail
-        for i in xrange(iter_n):
-            make_step(net, end=end, clip=clip, **step_params)
+        if clip:
+            bias = net.transformer.mean['data']
+            src.data[:] = np.clip(src.data, -bias, 255-bias)
 
-            # visualization
-            vis = deprocess(net, src.data[0])
-            if not clip: # adjust image contrast if clipping is disabled
-                vis = vis*(255.0/np.percentile(vis, 99.98))
-            #showarray(vis)
-            print octave, i, end, vis.shape
-            clear_output(wait=True)
+    def deepdream(net, base_img, iter_n=10, octave_n=4, octave_scale=1.4, end='inception_4c/output', clip=True, **step_params):
+        # prepare base images for all octaves
+        octaves = [preprocess(net, base_img)]
+        for i in xrange(octave_n-1):
+            octaves.append(nd.zoom(octaves[-1], (1, 1.0/octave_scale,1.0/octave_scale), order=1))
 
-        # extract details produced on the current octave
-        detail = src.data[0]-octave_base
-    # returning the resulting image
-    return deprocess(net, src.data[0])
+        src = net.blobs['data']
+        detail = np.zeros_like(octaves[-1]) # allocate image for network-produced details
+        for octave, octave_base in enumerate(octaves[::-1]):
+            h, w = octave_base.shape[-2:]
+            if octave > 0:
+                # upscale details from the previous octave
+                h1, w1 = detail.shape[-2:]
+                detail = nd.zoom(detail, (1, 1.0*h/h1,1.0*w/w1), order=1)
 
-if 'keys'==sys.argv[1]:
-    print "\n".join( net.blobs.keys())
-    sys.exit(0)
+            src.reshape(1,3,h,w) # resize the network's input image size
+            src.data[0] = octave_base+detail
+            for i in xrange(iter_n):
+                make_step(net, end=end, clip=clip, **step_params)
 
-img = np.float32(PIL.Image.open(sys.argv[1]))
-end_name = sys.argv[3] if len(sys.argv)>3 else 'inception_4c/output'
-iter_n = int(sys.argv[4]) if len(sys.argv)>4 else 10
-octaves = int(sys.argv[5]) if len(sys.argv)>5 else 4
-oct_scale = float(sys.argv[6]) if len(sys.argv)>6 else 1.4
-_=deepdream(net, img, end=end_name, iter_n=iter_n, octave_n=octaves, octave_scale=oct_scale)
-savearray(_, sys.argv[2])
+                # visualization
+                vis = deprocess(net, src.data[0])
+                if not clip: # adjust image contrast if clipping is disabled
+                    vis = vis*(255.0/np.percentile(vis, 99.98))
+                #showarray(vis)
+                print octave, i, end, vis.shape
+                clear_output(wait=True)
+
+            # extract details produced on the current octave
+            detail = src.data[0]-octave_base
+        # returning the resulting image
+        return deprocess(net, src.data[0])
+
+    if 'keys'==sys.argv[1]:
+        print "\n".join( net.blobs.keys())
+        sys.exit(0)
+
+    img = np.float32(PIL.Image.open(sys.argv[1]))
+    end_name = sys.argv[3] if len(sys.argv)>3 else 'inception_4c/output'
+    iter_n = int(sys.argv[4]) if len(sys.argv)>4 else 10
+    octaves = int(sys.argv[5]) if len(sys.argv)>5 else 4
+    oct_scale = float(sys.argv[6]) if len(sys.argv)>6 else 1.4
+    _=deepdream(net, img, end=end_name, iter_n=iter_n, octave_n=octaves, octave_scale=oct_scale)
+    savearray(_, sys.argv[2] + "." + str(foo).zfill(4) + ".png")
